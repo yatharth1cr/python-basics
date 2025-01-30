63b639879df2e97dbbcbc70a89bb39d5051df90a
yatharth1cr <yatharthgiri187@gmail.com>
2025-01-31 00:18:30 +0530

    completed some task of variable-constant assignments

---
+++ Variable-Constant/constant.py
@@ -0,0 +1,4 @@
+# declaring and assigning value to a constant
+PI = 3.14
+GRAVITY = 9.8   
+print(PI)
---
+++ Variable-Constant/variable.py
@@ -0,0 +1,18 @@
+#Declaring and assigning a value to a variable
+a = "Apple"
+print(a)
+
+#changing the value of a variable
+a = "Banana"
+print(a)
+
+a= 10
+print(a)
+
+#Assigning multiple values to a variables
+b, c, d = 5, 3.2, "Hello"
+print(b, c, d)
+
+#Assigning the same value to multiple variables
+b = c = d = 1
+print(b, c, d)
